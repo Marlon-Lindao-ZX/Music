2d3129558cef0e00010c94dd2ef96e84c97a7615
Marlon-Lindao-ZX <melindao@espol.edu.ec>
2021-06-18 12:14:55 -0500

    etc

--- music.c
+++ music.c
@@ -1,15 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "music.h"
 
+typedef unsigned char char1;
 
+void prueba(ej_t *ejemplo){
+
+    ejemplo->x = 20 * 5;
+    ejemplo->y = 10.5;
+    ejemplo->p = ejemplo;
+}
 
 int main(int argc, char **argv){
 
-    play_music(NULL);
+    ej_t ejemplo = {0};
+
+    char1 x;
+
+    ejemplo.x = 20;
+    ejemplo.y = 20.5;
+    ejemplo.p = &ejemplo;
+
+    /*
+
+    printf("%d\n", ejemplo.x);
+    printf("%lf\n",ejemplo.y);
+    printf("%p\n", ejemplo.p);
+
+    prueba(&ejemplo);
+
+    printf("%d\n", ejemplo.x);
+    printf("%lf\n",ejemplo.y);
+    printf("%p\n", ejemplo.p);
+    */
+
+    int opt;
+    arc_t archivo = {0};
+
+    while((opt = getopt(argc, argv, "f:e:")) > 0){
+        switch(opt){
+            case 'f':
+                strcpy(archivo.nombre,optarg);
+                break;
+            case 'e':
+                strcpy(archivo.extension,optarg);
+                break;
+            default:
+                printf("Opción no reconocida");
+                exit(1);
+        }
+    }
+
+    play_music(&archivo);
+    
+
+    return 0;
     
 }
 
-void play_music(char *music){
-    system("mpg321 1.mp3");
+
+
+void play_music(arc_t *archivo){
+    printf("%s.%s\n", archivo->nombre, archivo->extension);
+
+
+    //system("mpg321 1.mp3");
 }
 
--- music.h
+++ music.h
@@ -1,4 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
 
-void play_music(char *music);
+#define MAX 125
+
+typedef struct Archivo{
+    char nombre[MAX];
+    char extension[10];
+} arc_t;
+
+typedef struct Ejemplo{
+    int x;
+    double y;
+    void *p;
+} ej_t;
+
+void play_music(arc_t *archivo);
